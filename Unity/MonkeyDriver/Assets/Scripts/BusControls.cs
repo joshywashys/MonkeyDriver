48fde074beaee6935cadaee268c9d00f135b32c2
juliannaclarke <juliannaclarke@cmail.carleton.ca>
2021-10-28 01:00:14 -0400

    Added more Bus and Monkey logic
    I've decided that the bus will control it's position but the map matrix will pull it every so often to check for collisions and out of bounds cases.
    
    We currently lack a way to manage the controls and what controls would be available (e.g fading an option when it is not viable).

--- Unity/MonkeyDriver/Assets/Scripts/BusControls.cs
+++ Unity/MonkeyDriver/Assets/Scripts/BusControls.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 /*
 This script is responsible for:
 -each control that the bus can take (to be used by the monkey)
--collision with obstacles
+-bus is maintaining it's own position
+-bus is a singleton
+-map pulls the bus location
+-map checks if the bus is at the boundary and changes the property for the bus
 */
 enum Controls
 {
@@ -19,53 +22,19 @@ enum Controls
 }
 public class BusControls : MonoBehaviour
 {
-
     bool atBoundUp, atBoundLeft, atBoundRight, atBoundDown;
     bool hasPlow = false;
-    public float speed;
-    Controls[] activeControls = new Controls[3];
-    public void Up()
-    {
-        if (!atBoundUp)
-        {
-            //move up
-        }
-    }
+    public float curSpeed, maxSpeed, speedIncrement;
+    public static int numControls = 3;
 
-    public void Down()
-    {
+    List <Controls> activeControls = new List<Controls>;
 
-    }
-
-    public void Left()
-    {
-
-    }
-
-    public void Right()
-    {
-
-    }
-
-    public void Plow()
-    {
-
-    }
-
-    public void Rest()
-    {
-
-    }
-
-    public void Accelerate()
-    {
-
-    }
-
-    // Start is called before the first frame update
     void Start()
     {
-        
+        activeControls.Add(Controls.Up);
+        activeControls.Add(Controls.Right);
+        activeControls.Add(Controls.Left);
+
     }
 
     // Update is called once per frame
@@ -73,4 +42,83 @@ public class BusControls : MonoBehaviour
     {
         
     }
+    public void Up()
+    {
+        if (!atBoundUp)//this check will become obsolete since the control should never be called if it would result in out of bounds
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
+        }
+    }
+
+    public void Down()
+    {
+        if (!atBoundDown)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - 1, transform.position.z);
+        }
+    }
+
+    public void Left()
+    {
+        if (!atBoundLeft)
+        {
+            transform.position = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z);
+        }
+    }
+
+    public void Right()
+    {
+        if (!atBoundRight)
+        {
+            transform.position = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
+        }
+    }
+
+    public void Plow()
+    {
+        hasPlow = !hasPlow;
+    }
+
+    public void Rest()
+    {
+        //set speed to zero?
+    }
+
+    public void Accelerate()
+    {
+        if (curSpeed < maxSpeed)
+        {
+            curSpeed += speedIncrement;//add whatever factor we determine is justifiable
+        }
+    }
+
+    void executeAction(Controls control)
+    {
+        switch (control)
+        {
+            case Controls.Up:
+                Up();
+                break;
+            case Controls.Down:
+                Down();
+                break;
+            case Controls.Left:
+                Left();
+                break;
+            case Controls.Right:
+                Right();
+                break;
+            case Controls.Plow:
+                Plow();
+                break;
+            case Controls.Rest:
+                Rest();
+                break;
+            case Controls.Accelerate:
+                Accelerate();
+                break;
+        }
+    }
+
+
 }
--- Unity/MonkeyDriver/Assets/Scripts/MonkeyController.cs
+++ Unity/MonkeyDriver/Assets/Scripts/MonkeyController.cs
@@ -10,7 +10,8 @@ This script is responsible for:
 */
 public class MonkeyController : MonoBehaviour
 {
-
+    //SET THIS TO THE REAL NUMBER FROM THE BUS
+    int numControls = 3;
     void Start()
     {
         
@@ -19,6 +20,16 @@ public class MonkeyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //if at an intersection, make a control choice
+    }
+
+    int chooseControl()
+    {
+        //check for banana
+        //check for if a control would result in out of bounds
+        //eg. can't go up
+        //then the control set must be reduced to the number of viable options
+        int monkeyChoice = Random.Range(0, numControls);
+        return (monkeyChoice);
     }
 }
